80cc683d0f9ab5a924d8a7c530af6ffc84ac1f94
Disha <dishaagrawal428@gmail.com>
2026-09-25 00:57:08 +0530

    Fix ChromaDB locking issue

--- app.py
+++ app.py
@@ -1,158 +1,8 @@
-# import os
-# import streamlit as st
-# from dotenv import load_dotenv
-
-# from langchain_community.vectorstores import Chroma
-# from langchain_mistralai import ChatMistralAI
-# from langchain_mistralai import MistralAIEmbeddings
-# from langchain_core.prompts import ChatPromptTemplate
-
-# from create_database import create_vector_database
-
-# load_dotenv()
-
-# st.set_page_config(
-#     page_title="Multi PDF Chatbot",
-#     page_icon="📚",
-#     layout="wide"
-# )
-
-# st.title("📚 Multi PDF Chatbot")
-
-# uploaded_files = st.file_uploader(
-#     "Upload PDF(s)",
-#     type="pdf",
-#     accept_multiple_files=True
-# )
-
-# if uploaded_files:
-
-#     os.makedirs("uploads", exist_ok=True)
-
-#     pdf_paths = []
-
-#     for uploaded_file in uploaded_files:
-
-#         pdf_path = os.path.join(
-#             "uploads",
-#             uploaded_file.name
-#         )
-
-#         with open(pdf_path, "wb") as f:
-#             f.write(uploaded_file.getbuffer())
-
-#         pdf_paths.append(pdf_path)
-
-#     st.write("### Uploaded PDFs")
-
-#     for file in uploaded_files:
-#         st.write("✅", file.name)
-
-#     if st.button("Create Knowledge Base"):
-#         print("Calling Function...")
-#         print(pdf_paths)
-
-#         with st.spinner("Creating Embeddings..."):
-
-#             create_vector_database(pdf_paths)
-
-#         st.success("Knowledge Base Created!")
-
-#         st.session_state["db_ready"] = True
-
-
-# if st.session_state.get("db_ready", False):
-
-#     embedding_model = MistralAIEmbeddings(
-#         model="mistral-embed"
-#     )
-
-#     vectorstore = Chroma(
-#         persist_directory="chroma_db",
-#         embedding_function=embedding_model
-#     )
-
-#     retriever = vectorstore.as_retriever(
-#         search_type="mmr",
-#         search_kwargs={
-#             "k": 4,
-#             "fetch_k": 10,
-#             "lambda_mult": 0.5
-#         }
-#     )
-
-#     llm = ChatMistralAI(
-#         model="mistral-small-2506"
-#     )
-
-#     prompt = ChatPromptTemplate.from_messages([
-#         (
-#             "system",
-#             """
-# You are a helpful AI assistant.
-
-# Use ONLY the provided context.
-
-# If the answer is not present in the context,
-# say:
-# 'I could not find the answer in the uploaded documents.'
-# """
-#         ),
-#         (
-#             "human",
-#             """
-# Context:
-# {context}
-
-# Question:
-# {question}
-# """
-#         )
-#     ])
-
-#     question = st.text_input("Ask a Question")
-
-#     if st.button("Ask"):
-
-#         docs = retriever.invoke(question)
-
-#         context = "\n\n".join(
-#             [doc.page_content for doc in docs]
-#         )
-
-#         final_prompt = prompt.invoke({
-#             "context": context,
-#             "question": question
-#         })
-
-#         with st.spinner("Thinking..."):
-
-#             response = llm.invoke(final_prompt)
-
-#         st.subheader("Answer")
-
-#         st.write(response.content)
-
-#         with st.expander("Retrieved Chunks"):
-
-#             for i, doc in enumerate(docs, start=1):
-
-#                 st.markdown(f"### Chunk {i}")
-
-#                 st.write("📄 Source :", doc.metadata.get("source"))
-
-#                 st.write("📃 Page :", doc.metadata.get("page"))
-
-#                 st.write(doc.page_content)
-
-#                 st.divider()
-
 import os
 import streamlit as st
 from dotenv import load_dotenv
-
 from langchain_community.vectorstores import Chroma
-from langchain_mistralai import ChatMistralAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mistralai import MistralAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -205,12 +55,14 @@ if uploaded_files:
 
         with st.spinner("Creating Embeddings..."):
 
-            create_vector_database(pdf_paths)
+            db_path=create_vector_database(pdf_paths)
 
-        st.success("Knowledge Base Created!")
-
-        st.session_state["db_ready"] = True
-
+        if db_path is not None:
+            st.success("Knowledge Base Created!")
+    
+            st.session_state["db_ready"] = True
+            st.session_state["db_path"] = db_path
+    
 
 if st.session_state.get("db_ready", False):
 
@@ -219,7 +71,7 @@ if st.session_state.get("db_ready", False):
     )
 
     vectorstore = Chroma(
-        persist_directory="chroma_db",
+        persist_directory=st.session_state["db_path"],
         embedding_function=embedding_model
     )
 
@@ -232,9 +84,9 @@ if st.session_state.get("db_ready", False):
         }
     )
 
-    llm = ChatMistralAI(
-        model="mistral-small-2506"
-    )
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-3.6-flash"
+    ) 
 
     prompt = ChatPromptTemplate.from_messages([
         (
@@ -303,18 +155,29 @@ say:
             "question": question
         })
 
+        import time
+
+        # test_start = time.time()
+
+        # test_response = llm.invoke("Say hello in one sentence.")
+
+        # print("Simple Gemini time:", time.time() - test_start)
+        # print(test_response.text)
+
+        llm_start=time.time()
         with st.spinner("Thinking..."):
 
             response = llm.invoke(final_prompt)
 
+        print("LLM time:", time.time() - llm_start)
         with st.chat_message("assistant"):
 
-            st.markdown(response.content)
+            st.markdown(response.text)
 
         st.session_state.messages.append(
                 {
                   "role": "assistant",
-                  "content": response.content
+                  "content": response.text
               }
             )
 
@@ -335,4 +198,4 @@ say:
 
              st.write(doc.page_content)
 
-             st.divider()
+             st.divider()             
--- create_database.py
+++ create_database.py
@@ -1,6 +1,5 @@
 import os
-import shutil
-
+import uuid
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -11,16 +10,25 @@ load_dotenv()
 
 
 def create_vector_database(pdf_paths):
+    
 
     print("=" * 50)
     print("Function called")
     print("pdf_paths =", pdf_paths)
     print("=" * 50)
-
-    if os.path.exists("chroma_db"):
-        shutil.rmtree("chroma_db")
-
     
+
+    db_path = f"chroma_db_{uuid.uuid4().hex}"
+    # db_path = "chroma_db_new"
+
+    # if os.path.exists("chroma_db"):
+    #     try:
+    #         shutil.rmtree("chroma_db")
+    #     except PermissionError:
+    #         print("chroma_db is locked.Please restart the app")
+    #         return None    
+       
+
 
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
@@ -50,7 +58,9 @@ def create_vector_database(pdf_paths):
     vectorstore = Chroma.from_documents(
         documents=all_chunks,
         embedding=embedding_model,
-        persist_directory="chroma_db"
+        persist_directory=db_path
     )
 
-    return vectorstore
+    # return vectorstore
+
+    return db_path
